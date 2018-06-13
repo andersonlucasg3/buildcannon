7a8d0bd9e0cf096eecf0ef0f1dd24de976f2ae40
Anderson Lucas C. Ramos <aramos@horizonte.tv.br>
2018-06-13 14:15:54 -0300

    Initial Commit

---
+++ buildtool/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  buildtool
+//
+//  Created by Anderson Lucas C. Ramos on 13/06/18.
+//  Copyright © 2018 Anderson Lucas C. Ramos. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
