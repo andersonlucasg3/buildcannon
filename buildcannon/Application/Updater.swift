c7158810c481d99b900ffc7623fe9726a5480040
Anderson Lucas C. Ramos <anderson@insanitech.com.br>
2018-07-05 15:33:14 -0300

    Adicionado fonte do updater;

---
+++ buildcannon/Application/Updater.swift
@@ -0,0 +1,9 @@
+//
+//  Updater.swift
+//  buildcannon
+//
+//  Created by Anderson Lucas de Castro Ramos on 05/07/18.
+//  Copyright © 2018 InsaniTech. All rights reserved.
+//
+
+import Foundation
