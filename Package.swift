cccd4fee2fd3fbe7d185e8b4b2929c3a7de28fac
Anderson Lucas C. Ramos <aramos@corp.globo.com>
2019-01-29 15:58:11 -0200

    SPM
    - Atualizando description para versão 4 do SPM;

--- Package.swift
+++ Package.swift
@@ -1,5 +1,24 @@
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package.init(
-    name: "buildcannon"
+    name: "buildcannon",
+    products: [
+        .executable(
+            name: "buildcannon",
+            targets: ["buildcannon"]
+        )
+    ],
+    dependencies: [ ],
+    targets: [
+        .target(
+            name: "buildcannon",
+            dependencies: [],
+            path: "buildcannon"),
+        .testTarget(
+            name: "buildcannonTest",
+            dependencies: [],
+            path: "buildcannonTest")
+    ]
 )
+
